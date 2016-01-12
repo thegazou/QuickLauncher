8419a390f08d34b5d6934da359ed35a3b5f70254
Nicolas Gonin <nicolas.gonin@he-arc.ch>
2016-01-12 14:54:27 +0100

    Update de l'option "Shake"
    L'option permettant d'activer ou de désactiver le "shake" pour effacer
    l'historique est fonctionnel.

--- app/src/main/java/ch/hearc/motioncontrol/listeners/ShakeListener.java
+++ app/src/main/java/ch/hearc/motioncontrol/listeners/ShakeListener.java
@@ -1,8 +1,10 @@
 package ch.hearc.motioncontrol.listeners;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.preference.PreferenceManager;
 import android.util.FloatMath;
 
 import ch.hearc.motioncontrol.interfaces.OnShakeListener;
@@ -20,12 +22,15 @@ public class ShakeListener implements SensorEventListener {
     private static final int SHAKE_SLOP_TIME_MS = 500;
     private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
 
+    private boolean enable;
     private OnShakeListener mListener;
     private long mShakeTimestamp;
     private int mShakeCount;
+    SharedPreferences prefs;
 
-    public void setOnShakeListener(OnShakeListener listener) {
+    public void setOnShakeListener(SharedPreferences prefs, OnShakeListener listener) {
         this.mListener = listener;
+        this.prefs=prefs;
     }
 
 
@@ -36,35 +41,36 @@ public class ShakeListener implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (prefs.getBoolean("enable-shake-device-to-erase-history", true)){
+            if (mListener != null) {
+                double x = event.values[0];
+                double y = event.values[1];
+                double z = event.values[2];
 
-        if (mListener != null) {
-            double x = event.values[0];
-            double y = event.values[1];
-            double z = event.values[2];
+                double gX = x / SensorManager.GRAVITY_EARTH;
+                double gY = y / SensorManager.GRAVITY_EARTH;
+                double gZ = z / SensorManager.GRAVITY_EARTH;
 
-            double gX = x / SensorManager.GRAVITY_EARTH;
-            double gY = y / SensorManager.GRAVITY_EARTH;
-            double gZ = z / SensorManager.GRAVITY_EARTH;
+                // gForce will be close to 1 when there is no movement.
+                double gForce = Math.sqrt(gX * gX + gY * gY + gZ * gZ);
 
-            // gForce will be close to 1 when there is no movement.
-            double gForce = Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+                if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                    final long now = System.currentTimeMillis();
+                    // ignore shake events too close to each other (500ms)
+                    if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                        return;
+                    }
 
-            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
-                final long now = System.currentTimeMillis();
-                // ignore shake events too close to each other (500ms)
-                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
-                    return;
+                    // reset the shake count after 3 seconds of no shakes
+                    if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                        mShakeCount = 0;
+                    }
+
+                    mShakeTimestamp = now;
+                    mShakeCount++;
+
+                    mListener.onShake(mShakeCount);
                 }
-
-                // reset the shake count after 3 seconds of no shakes
-                if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
-                    mShakeCount = 0;
-                }
-
-                mShakeTimestamp = now;
-                mShakeCount++;
-
-                mListener.onShake(mShakeCount);
             }
         }
     }
